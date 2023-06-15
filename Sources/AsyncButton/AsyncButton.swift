5d5012759cde034861cc1cae8a437448e5fa6474
Edon <edon@valdman.works>
2023-06-15 15:10:29 -0500

    Update AsyncButton.swift
    Added initial draft

--- Sources/AsyncButton/AsyncButton.swift
+++ Sources/AsyncButton/AsyncButton.swift
@@ -8,5 +8,29 @@
 import SwiftUI
 
 public struct AsyncButton<Label: View, Trigger: Equatable>: View {
+    private let action: () async -> Void
+    private let label: Label
     
+    @State private var isRunning = false
+    @State private var task: Task<Void, Never>?
+    
+    public init(
+        action: @escaping () async -> Void,
+        @ViewBuilder label: () -> Label
+    ) {
+        self.action = action
+        self.label = label()
+    }
+    
+    public var body: some View {
+        Button {
+            task = Task {
+                isRunning = true
+                await action()
+                isRunning = false
+            }
+        } label: {
+            label
+        }
+    }
 }
