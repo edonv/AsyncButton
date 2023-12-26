6d11c13f0de87ccfb73f6e7497a3c638e0dca292
Edon <edon@valdman.works>
2023-12-26 13:05:46 -0600

    Update AsyncButtonTests.swift
    Commented-out testing code.

--- Tests/AsyncButtonTests/AsyncButtonTests.swift
+++ Tests/AsyncButtonTests/AsyncButtonTests.swift
@@ -6,6 +6,6 @@ final class AsyncButtonTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AsyncButton().text, "Hello, World!")
+//        XCTAssertEqual(AsyncButton().text, "Hello, World!")
     }
 }
