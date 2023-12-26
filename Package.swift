407d18e2f610b4e7a817871d4fa9085af5f206ef
Edon <edon@valdman.works>
2023-12-26 13:18:55 -0600

    Update Package.swift
    Temporarily commented-out the `.macOS` platform, as it's having issues testing out the preview on macOS.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "AsyncButton",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15),
+//        .macOS(.v10_15),
         .macCatalyst(.v13),
         .tvOS(.v13),
         .watchOS(.v6)
