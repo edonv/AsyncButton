0a9702f8c5a5f3058dfeef343088e05a647a03dc
Edon <edon@valdman.works>
2024-03-19 16:38:26 -0500

    Added empty privacy manifest

--- Package.swift
+++ Package.swift
@@ -27,7 +27,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AsyncButton",
-            dependencies: ["SwiftUIBackports"]),
+            dependencies: ["SwiftUIBackports"],
+            resources: [.copy("PrivacyInfo.xcprivacy")]
+        ),
         .testTarget(
             name: "AsyncButtonTests",
             dependencies: ["AsyncButton"]),
